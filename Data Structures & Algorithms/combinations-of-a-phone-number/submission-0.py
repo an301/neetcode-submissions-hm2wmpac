73f70663f33323d3853664dc4614e0b2fbe47d8d
Anish <anish.nambi@gmail.com>
2026-07-26 16:26:00 -0700

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {"2":['a','b','c'],
+                 "3":['d','e','f'],
+                 "4":['g','h','i'],
+                 "5":['j','k','l'],
+                 "6":['m','n','o'],
+                 "7":['p','q','r','s'],
+                 "8":['t','u','v'],
+                 "9":['w','x','y','z'],}
+
+        result = []
+
+        def helper(idx, curr):
+            if idx == len(digits):
+                result.append("".join(curr))
+                return
+            
+            for letter in phone[digits[idx]]:
+                curr.append(letter)
+                helper(idx + 1, curr)
+                curr.pop()
+            
+
+        helper(0, [])
+        if result == [""]:
+            return []
+        return result
